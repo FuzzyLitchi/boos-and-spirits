4f0058b637125f10009056062ce20549e0468f25
Polly Nielsen Boutet-Livoff <pollyboutet@gmail.com>
2021-03-29 15:26:55 +0200

    Add web support

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 use bevy::{prelude::*, render::camera::Camera};
 
 fn main() {
-    App::build()
-        .add_plugins(DefaultPlugins)
-        // .add_plugin(EasingsPlugin)
+    let mut app = App::build();
+    app.add_plugins(DefaultPlugins);
+
+    #[cfg(target_arch = "wasm32")]
+    app.add_plugin(bevy_webgl2::WebGL2Plugin);
+
+    app // .add_plugin(EasingsPlugin)
         .add_plugin(GamePlugin)
         .run();
 }
